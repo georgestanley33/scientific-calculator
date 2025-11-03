924091f937e6ca5f4d644dffe88a7ba7d968b0ed
George <georgestanley33@gmail.com>
2025-11-03 14:21:51 +0000

    delete files that werent meant to be in repo

--- ListsExtension.py
+++
@@ -1,8 +0,0 @@
-cities = ["London" , "Dublin" , "Halifax" , "Bradford" , "Leeds" , "Nottingham" , "Newcastle" , "Liverpool"]
-print(cities)
-userInputA = input("Enter the item you want to add to the list: ")
-cities.append(userInputA)
-userInputR = input("Enter the item you want to remove from the list: ")
-cities.remove(userInputR)
-cities.sort()
-print(f"Here is the sorted list: {cities}")
--- StudentsList.py
+++
@@ -1,7 +0,0 @@
-students = ["Amir" , "Sarah" , "Tom" , "Aisha" , "Leo"] # declares initial list
-students.append("John")
-students.append("Roger") # adds 2 students to the end of the list
-students.remove("Amir") # removes specified student
-students.insert(0,"Garry") # adds garry to the start of the list
-sortedStudents = students.sort() # sorts the list alphabetically
-print(f"Current students: {students}")
